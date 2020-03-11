2b1ef597a5a9c3acf5c2a76b40d237b90fc30941
Andrew Smith <ajs3993@gmail.com>
2020-03-11 14:13:44 -0500

    router added and rendered from index

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './Header.css';
 
-class Header extends React.Component {
-    render(){
-        return(
+const Header = props => (
+
+        
             <>
                 <header className='top'>
                     <h1>World Famous Bar-B-Que</h1>
                 </header>
                 <h3 className='tagline'>
-                    <span>{this.props.tagline}</span>
+                    <span>{props.tagline}</span>
                 </h3>
             </>
         )
-    }
-}
+    
 
 export default Header;
