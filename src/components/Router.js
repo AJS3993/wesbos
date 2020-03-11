0987e953d9a85cbdb013c41556c6eaa613c60224
Andrew Smith <ajs3993@gmail.com>
2020-03-11 14:36:18 -0500

    not found component and StorePicker handleClick

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import StorePicker from './StorePicker/StorePicker';
 import App from '../App';
+import NotFound from './NotFound/NotFound';
 
 const Router = () => (
     
@@ -10,6 +11,7 @@ const Router = () => (
 
             <Route exact path='/' component={StorePicker} />
             <Route path='/store/:storeId' component={App} />
+            <Route component={NotFound} />
 
         </Switch>
     </BrowserRouter>
--- src/components/StorePicker/StorePicker.js
+++ src/components/StorePicker/StorePicker.js
@@ -2,14 +2,24 @@ import React from 'react';
 import './StorePicker.css';
 
 class StorePicker extends React.Component {
+
+    handleClick(){
+        alert('heyyyyyyyyyy')
+    }
     
     render(){
         return (
             <>
                 <form className='formy'>
+                  
                   <h2>Enter a store</h2>
+
+                  <button onClick={this.handleClick}>THE BUTTON :0</button>
+                  
                   <input type='text' required placeholder='store name'></input>
+                  
                   <button type='submit'>Visit Store</button>
+                
                 </form>
             </>
         )
