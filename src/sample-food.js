a23903476965c8f218706eca7e020832c50268ae
Andrew Smith <ajs3993@gmail.com>
2020-03-23 12:49:00 -0500

    sample food file

---
+++ src/sample-food.js
@@ -0,0 +1,21 @@
+const dishes = {
+    dish1: {
+      name: "Pacific Halibut",
+      image: "/images/hali.jpg",
+      desc:
+        "Everyone’s favorite white fish. We will cut it to the size you need and ship it.",
+      price: 1724,
+      status: "available"
+    },
+  
+    dish2: {
+      name: "Lobster",
+      image: "/images/lobster.jpg",
+      desc:
+        "These tender, mouth-watering beauties are a fantastic hit at any dinner party.",
+      price: 3200,
+      status: "available"
+    },
+  };
+  
+  export default dishes;
