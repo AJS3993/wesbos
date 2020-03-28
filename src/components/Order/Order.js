1d6bc0b90a871fa7b2be82b71496e4d9973c5108
Andrew Smith <ajs3993@gmail.com>
2020-03-28 17:33:16 -0500

    count added to order

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,6 +3,16 @@ import './Order.css';
 import {formatPrice} from '../../helpers.js'
 
 class Order extends React.Component {
+    renderOrder = key => {
+        const dish = this.props.food[key];
+        const count = this.props.order[key];
+        return( <li>
+            {count} {dish.name}
+            {formatPrice(count * dish.price)}
+        </li>
+        )
+    }
+    
     render(){
 
         const orderIds = Object.keys(this.props.order);
@@ -21,10 +31,10 @@ class Order extends React.Component {
             <div className='order-wrap'>
                 <h2>Order</h2>
                 <ul>
-                    <li>{orderIds}</li>
+                    {orderIds.map(this.renderOrder)}
                 </ul>
                 <div className='total'>
-                    Total: {formatPrice(total)}
+                    Total: <strong>{formatPrice(total)}</strong>
                 </div>
             </div>
         )
