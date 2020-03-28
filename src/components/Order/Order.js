e96891c19ab46f16185071b51b15596bd1e40714
Andrew Smith <ajs3993@gmail.com>
2020-03-28 17:05:09 -0500

    order total calculated and rendered

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -4,7 +4,17 @@ import './Order.css';
 class Order extends React.Component {
     render(){
 
-        const orderIds = Object.keys(this.props.order)
+        const orderIds = Object.keys(this.props.order);
+
+        const total = orderIds.reduce((prevTotal, key) => {
+            const dish = this.props.food[key];
+            const count = this.props.order[key];
+            const isAvailable = dish && dish.status === 'available';
+            if(isAvailable){
+                return prevTotal + (count * dish.price)
+            }
+            return prevTotal
+        }, 0);
 
         return (
             <div className='order-wrap'>
@@ -12,6 +22,9 @@ class Order extends React.Component {
                 <ul>
                     <li>{orderIds}</li>
                 </ul>
+                <div className='total'>
+                    {total}
+                </div>
             </div>
         )
     }
