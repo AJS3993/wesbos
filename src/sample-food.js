1dc33733fad4b44b231c7b3fb8b939b066ff464c
Andrew Smith <ajs3993@gmail.com>
2020-04-04 14:32:47 -0500

    sample food update

--- src/sample-food.js
+++ src/sample-food.js
@@ -1,18 +1,26 @@
 const dishes = {
     dish1: {
-      name: "Lobster",
-      image: "https://raw.githubusercontent.com/wesbos/React-For-Beginners-Starter-Files/master/stepped-solutions/Finished%20App/public/images/lobster.jpg",
+      name: "Ribs",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRfydjxZvenBMP6325jaw0_K_hBv7qzpX7YSc5ntZ4Vx_ly2lVb&usqp=CAU",
       desc:
-        "Everyone’s favorite",
+        "Covered with our secret recipe rub then slow smoked for 4-6 hours over hardwood oak and apple woods to ‘melt-in-your-mouth’ perfection. Help yourself to the sauce of your choice.",
       price: 1499,
       status: "available"
     },
     dish2: {
-      name: "Cheeseburger",
-      image: "https://i.pinimg.com/originals/12/6c/00/126c00315061c37544409c60931d6bdb.jpg",
+      name: "BBQ Chicken",
+      image: "https://thestayathomechef.com/wp-content/uploads/2018/06/Baked-BBQ-Chicken-1-small-1-150x150.jpg",
       desc:
-        "Everyone’s second favorite",
+        "Halves, Quarters and Thighs are cooked in the smoker which brings you exceptional tenderness and flavor. Help yourself to the sauce of your choice tableside.",
       price: 800,
+      status: "available"
+    },
+    dish3: {
+      name: "Brisket",
+      image: "https://img.texasmonthly.com/2018/01/Black-Board-Bar-B-Q-2.jpg?auto=compress&crop=faces&fit=crop&fm=pjpg&h=150&ixlib=php-1.2.1&q=45&w=150&wpsize=thumbnail",
+      desc:
+        "Rubbed with our secret brisket rub then slow smoked 12-14 hours over hardwood oak and apple woods to a very tender and flavorful experience. Served stand alone, as a sandwich or in our famous meat combos, this juicy brisket is an explosion of flavor. Add the sauce of your choice tableside.",
+      price: 900,
       status: "available"
     }
   };
